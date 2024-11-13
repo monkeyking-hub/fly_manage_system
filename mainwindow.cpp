dd0be5a632b6756a6099bb3b0ec38b688fc5c207
eatbreads <1852611363@qq.com>
2024-11-14 00:00:42 +0800

    本地的第二次提交

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,10 +2,10 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent)//初始化父窗口,使得有那些功能
+    , ui(new Ui::MainWindow)//Ui:MainWindow是设计师界面生成的类
 {
-    ui->setupUi(this);
+    ui->setupUi(this);//这个ui就是那个生成的类的指针(Ui里面的MainWindow)
 }
 
 MainWindow::~MainWindow()
