ec690636a57581178183695340ebb0d0377219b3
Run <2771960775@qq.com>
2024-11-25 02:02:30 +0800

    给首页的按钮加了跳转功能，但还没做好

--- newhomewindow.cpp
+++ newhomewindow.cpp
@@ -1,5 +1,9 @@
 #include "newhomewindow.h"
 #include "ui_newhomewindow.h"
+#include "interfacemanager.h"
+#include <QCommandLinkButton>
+#include <QApplication>
+#include <QWidget>
 
 newHomeWindow::newHomeWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,7 +20,32 @@ newHomeWindow::newHomeWindow(QWidget *parent)
         "}"
         );
 
+    connect(ui->commandLinkButton_3,&QCommandLinkButton::clicked,this,&newHomeWindow::onCommandButton3Clicked);
+    connect(ui->commandLinkButton_4,&QCommandLinkButton::clicked,this,&newHomeWindow::onCommandButton4Clicked);
+    connect(ui->commandLinkButton_5,&QCommandLinkButton::clicked,this,&newHomeWindow::onCommandButton5Clicked);
+    connect(ui->commandLinkButton_6,&QCommandLinkButton::clicked,this,&newHomeWindow::onCommandButton6Clicked);
 }
+
+void newHomeWindow::onCommandButton3Clicked() //点击按钮后跳转到编辑个人信息界面
+{
+    InterfaceManager::instance()->switchToPage("fzj_window");
+}
+
+void newHomeWindow::onCommandButton4Clicked() //点击按钮后跳转到航班动态界面
+{
+    InterfaceManager::instance()->switchToPage("fzj_flightstatus");
+}
+
+void newHomeWindow::onCommandButton5Clicked() //点击按钮后跳转到订单界面
+{
+    InterfaceManager::instance()->switchToPage("wmc_orderWindow");
+}
+
+void newHomeWindow::onCommandButton6Clicked() //点击按钮后跳转到homewindow界面
+{
+    InterfaceManager::instance()->switchToPage("lxt_homeWindow");
+}
+
 newHomeWindow::~newHomeWindow()
 {
     delete ui;
--- newhomewindow.h
+++ newhomewindow.h
@@ -13,6 +13,10 @@ class newHomeWindow : public QMainWindow
 
 public:
     explicit newHomeWindow(QWidget *parent = nullptr);
+    void onCommandButton3Clicked(); //点击按钮后跳转到编辑个人信息界面
+    void onCommandButton4Clicked(); //点击按钮后跳转到航班动态界面
+    void onCommandButton5Clicked(); //点击按钮后跳转到订单界面
+    void onCommandButton6Clicked(); //点击按钮后跳转到homewindow界面
     ~newHomeWindow();
 
 private:
