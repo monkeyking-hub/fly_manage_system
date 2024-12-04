a836191936bea94e7c21afd9981a58b6f600ef22
Run-ux <2771960775@qq.com>
2024-12-04 15:59:38 +0800

    增加了管理员登录界面
    管理员登录界面类是adminLoginWindow

--- newregisterwindow.h
+++ newregisterwindow.h
@@ -33,8 +33,27 @@ public:
     explicit newRegisterWindow(QWidget *parent = nullptr);
     ~newRegisterWindow();
 
+signals:
+    void registerRequested(const QString &usrname,const QString& password,const QString& email,
+                           const QString &phone,const QString &idNumber,const int &age=-1,
+                           const QString &gender="M/F",const int &usrLevel=1);
+
+public slots:
+    void onRegisterButtonClicked();
+
 private:
     Ui::newRegisterWindow *ui;
+};
+
+
+class registerHandler:public QObject
+{
+    Q_OBJECT;
+
+public slots:
+    void handleRegister(const QString &usrname,const QString& password,const QString& email,
+                        const QString &phone,const QString &idNumber,const int &age=-1,
+                        const QString &gender="M/F",const int &usrLevel=1);
 };
 
 #endif // NEWREGISTERWINDOW_H
